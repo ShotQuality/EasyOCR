bc50d4a4d91758a936b4bdd4cb96eb4d224f661a
Wesley Passos <wesley@shotquality.com>
2023-11-01 19:05:51 +0000

    initial commit dataset conversion

---
+++ tools/check_sb2craft_convert.py
@@ -0,0 +1,69 @@
+from sq_datasets import ScoreboardDataset
+from pathlib import Path
+from tqdm import tqdm
+from PIL import Image, ImageDraw
+
+
+class CRAFTSBDataset:
+    def __init__(self, images_dir) -> None:
+        self.images_dir = images_dir
+        self.annotations_dir = images_dir.replace(
+            "images", "localization_transcription_gt"
+        )
+
+        self.list_images = list(Path(self.images_dir).glob("*.jpg"))
+
+    def __getitem__(self, index):
+        image_path = self.list_images[index]
+        image = Image.open(image_path)
+        annotation = self._get_annotation(index)
+
+        return image_path, image, annotation
+
+    def __len__(self):
+        return len(self.list_images)
+
+    def _get_annotation(self, index):
+        image_name = self.list_images[index].name
+        annotation_path = Path(self.annotations_dir) / image_name.replace(
+            ".jpg", ".txt"
+        )
+
+        with open(annotation_path, "r") as f:
+            lines = f.readlines()
+
+            annotations = []
+            for line in lines:
+                line = line.strip().split(",")
+                annotations.append(line)
+
+        return annotations
+
+
+def func_draw_boxes(image, box, width=2, color=(0, 255, 0)):
+    draw = ImageDraw.Draw(image)
+    draw.rectangle(box, width=width, outline=color)
+    return image
+
+
+if __name__ == "__main__":
+    dataset = CRAFTSBDataset(images_dir="/data/sb_craft_dataset/training_images")
+
+    for i, sample in enumerate(dataset):
+        image_path, sb, annotations = sample
+
+        for annotation in annotations:
+            xtl = int(annotation[0])
+            ytl = int(annotation[1])
+            xbr = int(annotation[4])
+            ybr = int(annotation[5])
+            text = annotation[-1]
+            sb_with_box = func_draw_boxes(sb, (xtl, ytl, xbr, ybr))
+
+        # uncomment the line below to save the images with bboxes
+        # sb_with_box.save(f"sb{i}.jpg")
+
+        if i > 5:
+            break
+
+    print("Done!")
---
+++ tools/convert_scoreboard2craft.py
@@ -0,0 +1,130 @@
+from sq_datasets import ScoreboardDataset
+from pathlib import Path
+from tqdm import tqdm
+from PIL import Image
+
+
+class CRAFTSBDataset:
+    def __init__(self, images_dir) -> None:
+        self.images_dir = images_dir
+        self.annotations_dir = images_dir.replace(
+            "images", "localization_transcription_gt"
+        )
+
+        self.list_images = list(Path(self.images_dir).glob("*.jpg"))
+
+    def __getitem__(self, index):
+        image = Image.open(self.list_images[index])
+        annotation = self._get_annotation(index)
+
+        return image, annotation
+
+    def __len__(self):
+        return len(self.list_images)
+
+    def _get_annotation(self, index):
+        image_name = self.list_images[index].name
+        annotation_path = Path(self.annotations_dir) / image_name.replace(
+            ".jpg", ".txt"
+        )
+
+        with open(annotation_path, "r") as f:
+            lines = f.readlines()
+
+        annotation = []
+        for line in lines:
+            line = line.strip().split(",")
+            annotation.append(line)
+
+        return annotation
+
+
+def get_scoreboard(sample):
+    frame = sample["image"]
+    sb_bbox = sample["boxes"]["xyx2y2_abs"][
+        sample["boxes"]["label"].index("Scoreboard")
+    ]
+    x1, y1, x2, y2 = sb_bbox
+    return frame.crop((x1, y1, x2, y2))
+
+
+def convert_annotations(image_annotations):
+    """
+    Convert the annotations from the format used in the scoreboard dataset to the format used in CRAFT
+    :param image_annotations: list of annotations for one image
+    :return: list of annotations for one image in CRAFT format
+    """
+    sb_box = image_annotations["xyx2y2_abs"][
+        image_annotations["label"].index("Scoreboard")
+    ]
+    sb_box_origin = [sb_box[0], sb_box[1]]
+
+    annotations = []
+    for text, box in zip(image_annotations["text"], image_annotations["xyx2y2_abs"]):
+        if text == "":
+            continue
+
+        xtl, ytl, xbr, ybr = box
+        # since we are croping the scoreboard, subtract the origin of the scoreboard box
+        xtl = xtl - sb_box_origin[0]
+        xbr = xbr - sb_box_origin[0]
+        ytl = ytl - sb_box_origin[1]
+        ybr = ybr - sb_box_origin[1]
+
+        box_4coords = [xtl, ytl, xbr, ytl, xbr, ybr, xtl, ybr]
+        box_4coords = list(map(lambda x: int(round(x)), box_4coords))
+
+        annotations.append([*box_4coords, text])
+
+    return annotations
+
+
+def write_annotation_file(craft_annotations, filepath):
+    # Open the file in write mode
+    with open(filepath, "w") as file:
+        for box in craft_annotations:
+            # Convert each element in the inner list to a string and join them with commas
+            line = ",".join(map(str, box))
+
+            # Write the line to the file
+            file.write(line + "\n")
+
+
+def scoreboard2craft(dataset, images_out_dir="/data/sb_craft_images"):
+    # TODO: ugly
+    out_images_dir = Path(images_out_dir)
+    out_annotations_dir = Path(
+        images_out_dir.replace("images", "localization_transcription_gt")
+    )
+
+    out_images_dir.mkdir(parents=True, exist_ok=True)
+    out_annotations_dir.mkdir(parents=True, exist_ok=True)
+
+    for i in tqdm(range(len(dataset))):
+        sample = dataset[i]
+        annotations = sample["boxes"]
+        image_path = Path(sample["image_path"])
+        image_name = image_path.name
+
+        # crop scoreboard region and save it
+        scoreboard_image = get_scoreboard(sample)
+        scoreboard_image.save(str(out_images_dir / image_name))
+
+        # convert annotations and save
+        annotations_craft = convert_annotations(annotations)
+        write_annotation_file(
+            annotations_craft,
+            str(out_annotations_dir / image_name.replace(".jpg", ".txt")),
+        )
+
+        if i > 5:  # TODO: remove!
+            break
+
+
+if __name__ == "__main__":
+    train_dataset = ScoreboardDataset.load_dataset(split="train")
+    scoreboard2craft(
+        train_dataset, images_out_dir="/data/sb_craft_dataset/training_images"
+    )
+
+    print("end")
